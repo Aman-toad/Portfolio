c552efba0323e701af57a91ef7b3519849ca5a88
Aman Singh <amanaspirantsingh@gmail.com>
2025-03-29 01:03:18 +0530

    let's see

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import { StrictMode } from 'react'
-import React from 'react';
-import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Portfolio">
     <App />
   </BrowserRouter>,
 )
