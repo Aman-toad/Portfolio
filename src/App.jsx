58143763285ae24676c4e42db419abca40db0597
Aman Singh <amanaspirantsingh@gmail.com>
2025-09-05 11:39:21 +0530

    overflow problem

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return(
-    <div>
+    <div className="min-h-screen">
       <Index/>
       <About />
       <Skills />
