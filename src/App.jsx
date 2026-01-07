4b6c7dfbe4bc413ea62a494b3575f9a55add9b3c
Aman Singh <amanaspirantsingh@gmail.com>
2026-01-07 14:05:56 +0530

    removed snowfall

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import Contact from "./sections/Contact";
 import Skills from "./sections/Skills.jsx";
 import Footer from "./components/Footer";
 import CustomCursor from "./components/CustomCursor.jsx";
-import Snowfall from "react-snowfall";
 
 export default function App() {
   return(
     <div className="min-h-screen">
-      <Snowfall color="#82C3D9"/>
       <CustomCursor/>
       <Index/>
       <Skills />
